69068023e87d52a68dc1aa2ef08abf0e2918436a
tfynes-pivotal <tfynes@pivotal.io>
2022-08-15 15:25:02 -0400

    Update create-workload.sh

--- create-workload.sh
+++ create-workload.sh
@@ -1,1 +1,1 @@
-tanzu apps workload create hp1x --git-repo https://github.com/tfynes-pivotal/hp1x --git-branch main --type web -y
+tanzu apps workload create hp1x --git-repo https://github.com/tfynes-pivotal/hp1x --label "app.kubernetes.io/part-of=hp1x" --git-branch main --type web -y
